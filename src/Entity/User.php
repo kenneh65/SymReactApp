0ce2a98cbe68461e94349f1505669ec7bffac341
kenneh <kenneh65@gmail.com>
2020-05-16 00:59:11 +0000

    Ajout de l'authentification via jwt

--- src/Entity/User.php
+++ src/Entity/User.php
@@ -13,7 +13,9 @@ use Symfony\Bridge\Doctrine\Validator\Constraints\UniqueEntity;
 
 /**
  * @ORM\Entity(repositoryClass="App\Repository\UserRepository")
- * @ApiResource
+ * @ApiResource(
+ * normalizationContext={"groups"={"users_read"}}
+ * )
  * @UniqueEntity(fields="email",message="Un utilisateur ayant cette adresse email existe deja")
  */
 class User implements UserInterface
@@ -22,13 +24,13 @@ class User implements UserInterface
      * @ORM\Id()
      * @ORM\GeneratedValue()
      * @ORM\Column(type="integer")
-     * @Groups({"customers_resd","invoices_subresource"})
+     * @Groups({"customers_resd","invoices_subresource","users_read"})
      */
     private $id;
 
     /**
      * @ORM\Column(type="string", length=180, unique=true)
-     * @Groups({"customers_resd","invoices_read","invoices_subresource"})
+     * @Groups({"customers_resd","invoices_read","invoices_subresource","users_read"})
      * @Assert\NotBlank(message="L'email doit etre renseigner")
      * @Assert\Email(message="Le format de l'adresse email doit etre valide")
      */
@@ -48,7 +50,7 @@ class User implements UserInterface
 
     /**
      * @ORM\Column(type="string", length=255)
-     * @Groups({"customers_resd","invoices_read","invoices_subresource"})
+     * @Groups({"customers_resd","invoices_read","invoices_subresource","users_read"})
      * @Assert\NotBlank(message="Le prenom est obligatoire")
      * @Assert\Length(min=3,minMessage="Le prenom doit faire entre 3 et 225 caracteres",max=225,
      * maxMessage="Le prenom doit faire entre 3 et 225 caracteres")
@@ -57,7 +59,7 @@ class User implements UserInterface
 
     /**
      * @ORM\Column(type="string", length=255)
-     * @Groups({"customers_resd","invoices_read","invoices_subresource"})
+     * @Groups({"customers_resd","invoices_read","invoices_subresource","users_read"})
      * @Assert\NotBlank(message="Le nom est obligatoire")
      * @Assert\Length(min=3,minMessage="Le nom de famille doit faire entre 3 et 225 caracteres",max=225,
      * maxMessage="Le de famille doit faire entre 3 et 225 caracteres")
